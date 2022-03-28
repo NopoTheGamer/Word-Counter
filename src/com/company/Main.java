6621a4fd12ccbc3d76b6f1bcf0260b462ca96d25
NopoTheGamer <noahogno@gmail.com>
2022-03-28 18:53:27 +1100

    Added input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,70 +1,17 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static int[] charCount = new int[26];
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        String words = "We're no strangers to love\n" +
-                "You know the rules and so do I\n" +
-                "A full commitment's what I'm thinking of\n" +
-                "You wouldn't get this from any other guy\n" +
-                "I just wanna tell you how I'm feeling\n" +
-                "Gotta make you understand\n" +
-                "Never gonna give you up\n" +
-                "Never gonna let you down\n" +
-                "Never gonna run around and desert you\n" +
-                "Never gonna make you cry\n" +
-                "Never gonna say goodbye\n" +
-                "Never gonna tell a lie and hurt you\n" +
-                "We've known each other for so long\n" +
-                "Your heart's been aching but you're too shy to say it\n" +
-                "Inside we both know what's been going on\n" +
-                "We know the game and we're gonna play it\n" +
-                "And if you ask me how I'm feeling\n" +
-                "Don't tell me you're too blind to see\n" +
-                "Never gonna give you up\n" +
-                "Never gonna let you down\n" +
-                "Never gonna run around and desert you\n" +
-                "Never gonna make you cry\n" +
-                "Never gonna say goodbye\n" +
-                "Never gonna tell a lie and hurt you\n" +
-                "Never gonna give you up\n" +
-                "Never gonna let you down\n" +
-                "Never gonna run around and desert you\n" +
-                "Never gonna make you cry\n" +
-                "Never gonna say goodbye\n" +
-                "Never gonna tell a lie and hurt you\n" +
-                "Never gonna give, never gonna give\n" +
-                "(Give you up)\n" +
-                "We've known each other for so long\n" +
-                "Your heart's been aching but you're too shy to say it\n" +
-                "Inside we both know what's been going on\n" +
-                "We know the game and we're gonna play it\n" +
-                "I just wanna tell you how I'm feeling\n" +
-                "Gotta make you understand\n" +
-                "Never gonna give you up\n" +
-                "Never gonna let you down\n" +
-                "Never gonna run around and desert you\n" +
-                "Never gonna make you cry\n" +
-                "Never gonna say goodbye\n" +
-                "Never gonna tell a lie and hurt you\n" +
-                "Never gonna give you up\n" +
-                "Never gonna let you down\n" +
-                "Never gonna run around and desert you\n" +
-                "Never gonna make you cry\n" +
-                "Never gonna say goodbye\n" +
-                "Never gonna tell a lie and hurt you\n" +
-                "Never gonna give you up\n" +
-                "Never gonna let you down\n" +
-                "Never gonna run around and desert you\n" +
-                "Never gonna make you cry\n" +
-                "Never gonna say goodbye";
-        String dash = "-";
         String full = "⣿";
         String[] graph = new String[26];
+        System.out.println("What string would you like to check the char count of?");
+        String words = scanner.nextLine();
         char[] wordsArray = words.toCharArray();
         char[] alphabetArray = alphabet.toCharArray();
         StringBuilder graphString = new StringBuilder();
@@ -87,6 +34,7 @@ public class Main {
                         .append("%").append("\n");
             }
         }
+        System.out.println();
         System.out.println(graphString);
     }
 }
